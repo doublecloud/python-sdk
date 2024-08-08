fcca8b36cb2c729c2b8840c107e6be4aa8f3952d
Mikhail Epikhin <epikhinm@double.cloud>
2024-08-08 16:16:37 +0200

    Revert "Revert "0.11.0""
    This reverts commit bfe4aa2490c9549845948e6f3fbb240cf806c77e.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 packages = find_packages(".", include=["doublecloud*"])
 
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 
 setup(
     name="doublecloud",
